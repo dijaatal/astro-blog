f462e77971dd7dd3a0c3006ccd3ce2d3d2eae68d
Solocydev <134819058+Solocydev@users.noreply.github.com>
2023-08-15 21:39:29 -0700

    Finished Tutorial

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
-    site: 'https://my-blog-site.netlify.app',
+    title: 'Dijaatal | Blog',
+    description: 'Astro Blog',
+    site: 'https://frolicking-mochi-d01b52.netlify.app/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
